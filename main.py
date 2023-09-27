af0c9d34445848c314aed5e30a0871b41ffc2c6b
Matthew Vrbka <matthewjvrbka@gmail.com>
2023-09-27 12:03:33 -0500

    fixed the generation functions, forgot variable assignments

--- main.py
+++ main.py
@@ -3,31 +3,33 @@ import random
 import string
 
 rand_strings = []
+enc_strings = []
 shift_num = 0
+direct_num = 0
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+def encrypt():
+    global direct_num, shift_num, rand_strings, enc_strings
+
 
 def gen_direct_num():
-    global shift_num
-    random.randint(0, 1)
+    global direct_num
+    direct_num = random.randint(0, 1)
+
 
 def gen_shift_num():
     global shift_num
-    random.randint(1, 10)
+    shift_num = random.randint(1, 10)
 
 
 def create_strings():
     global rand_strings
     while len(rand_strings) < 50:
-        new_string = ''.join(random.choices(string.ascii_letters, k=5))
+        new_string = ''.join(random.choices(string.ascii_letters, k=7))
         rand_strings.append(new_string)
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
@@ -35,5 +37,6 @@ if __name__ == '__main__':
     create_strings()
     gen_direct_num()
     gen_shift_num()
+    encrypt()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
